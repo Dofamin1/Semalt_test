8eb4c572a0272ce29e11736fbeea01f486516efd
Anton <gugenot990@gmail.com>
2019-04-25 19:52:47 +0300

    cerated server

--- server/worker.js
+++ server/worker.js
@@ -1,11 +1,25 @@
 require("dotenv").config();
 const http = require("http");
 const pid = process.pid;
+const googleApi = require("./googleApi");
+
+const handlersList = {
+  "/lighthouseResult/audits": googleApi.getAuditData
+};
 
 http
-  .createServer((req, res) => {
-    res.write(`Handled by process ${pid}\n`);
-    res.end();
+  .createServer(async (req, res) => {
+    const handler = handlersList[req.url] || (() => null);
+    const data = await handler();
+    if (data) {
+      res.writeHead(200, {
+        "Content-Type": "application/json"
+      });
+      res.end(data);
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
   })
   .listen(process.env.PORT, () => {
     console.log(`Started server, pid - ${pid}`);
