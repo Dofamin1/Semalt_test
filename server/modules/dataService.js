0fb98572e1b017ca4f1769480a15cd374ceab265
Anton <gugenot990@gmail.com>
2019-04-26 13:02:55 +0300

    added checksum verification

--- server/modules/dataService.js
+++ server/modules/dataService.js
@@ -1,17 +1,29 @@
 const axios = require("axios");
+var crypto = require("crypto");
 
 class DataService {
   constructor() {
     this.url =
       "https://www.googleapis.com/pagespeedonline/v5/runPagespeed/?url=https://habr.com/";
     this.interval = 10000;
+    this.checksum = null;
+  }
+  generateChecksum(str) {
+    return crypto
+      .createHash("md5")
+      .update(str, "utf8")
+      .digest("hex");
   }
   getAndSaveAuditData(redis) {
     return axios
       .get(this.url)
       .then(({ data }) => {
         const strigifiedData = JSON.stringify(data.lighthouseResult.audits);
-        return redis.setAsync("data", strigifiedData);
+        const newChecksum = this.generateChecksum(strigifiedData);
+        if (this.checksum !== newChecksum) {
+          this.checksum = newChecksum;
+          return redis.setAsync("data", strigifiedData);
+        }
       })
       .catch(e => {
         console.error("Error -", e);
