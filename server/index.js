ff7ea31bf35d0b7daea33fa5b987e1a46f9b4846
Anton <gugenot990@gmail.com>
2019-04-26 12:29:47 +0300

    developed master thread of cluster

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-require("dotenv").config();
 const cluster = require("cluster");
-const os = require("os");
+const DataService = require("./modules/dataService");
+const redisController = require("./modules/redis");
 
 if (cluster.isMaster) {
+  const os = require("os");
+  const dataService = new DataService();
   const cpus = os.cpus().length;
-  for (let i = 0; i < cpus; i++) {
-    cluster.fork();
-  }
+
+  redisController.createInstance(async () => {
+    console.log("wait until data be retrieved");
+    await dataService.getAndSaveAuditData(redisController.instance);
+    for (let i = 0; i < cpus; i++) {
+      cluster.fork();
+    }
+    dataService.startProcessingData(redisController.instance);
+  });
 } else {
-  require("./worker.js");
+  require("./serverWorker.js");
 }
