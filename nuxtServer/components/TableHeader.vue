dd8b93b1b27e24a0638d2c0ef2c93603f06ababa
Anton <gugenot990@gmail.com>
2019-04-28 16:29:04 +0300

    created new component - TableHeader

---
+++ nuxtServer/components/TableHeader.vue
@@ -0,0 +1,34 @@
+<template >
+  <v-layout row wrap>
+    <v-flex class="text-xs-center" xs4>
+      <span>{{auditType}}</span>
+    </v-flex>
+    <v-flex class="text-xs-center" xs4>
+      <span>{{metricName}}</span>
+    </v-flex>
+  </v-layout>
+</template>
+    
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "TableHeader",
+  computed: {
+    ...mapState(["activeMenuItem"]),
+    auditType() {
+      return this.activeMenuItem == "byOpportunity"
+        ? "Opportunity"
+        : "Audit title";
+    },
+    metricName() {
+      if (this.activeMenuItem == "byOpportunity") return "Estimated savings";
+      else if (this.activeMenuItem == "byScore") return "Score (from 0 to 1)";
+      else return null;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
